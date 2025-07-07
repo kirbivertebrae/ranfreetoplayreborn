3405353ea872c4366441454058f91464de9bc6f6
kirbivertebrae <kirbicarlon@gmail.com>
2025-07-06 20:31:09 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let slideIndex = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlides() {
+  slides.forEach(slide => {
+    slide.style.display = 'none';
+  });
+
+  slideIndex++;
+  if (slideIndex > slides.length) slideIndex = 1;
+
+  slides[slideIndex - 1].style.display = 'block';
+  setTimeout(showSlides, 3500);
+}
+
+document.addEventListener("DOMContentLoaded", showSlides);
